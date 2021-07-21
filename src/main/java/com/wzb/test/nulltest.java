a3ba2d266b5818d4a8a7d91ec2b0cede4bc98a23
2533585281 <2533585281@qq.com>
2021-07-21 10:16:47 +0800

    空指针捕获测试,一个计算器

---
+++ src/main/java/com/wzb/test/nulltest.java
@@ -0,0 +1,24 @@
+package com.wzb.test;
+
+/**
+ * @Author Administrator
+ * @Date 2021/7/21 0021 9:54
+ */
+public class nulltest {
+
+
+    public static void main(String[] args) {
+        String ss=null;
+        try{
+            if(ss.equals("a")){
+                System.out.println("a");
+            }
+
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            System.out.println("报错了");
+        }
+    }
+
+
+}
